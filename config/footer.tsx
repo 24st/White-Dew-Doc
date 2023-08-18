c335e857219ffd70cfa868ee95be799adb0931bc
accums <accums@pm.me>
2023-08-18 14:26:59 +0800

    Update V0.1

---
+++ config/footer.tsx
@@ -0,0 +1,7 @@
+export const footer = '' +
+  '' +
+  'Theme by <a target="_blank" href="https://github.com/KuangPF/dumi-theme-antd">dumi-theme-antd</a>' +
+  ' | ' +
+  'Copyright © 2023 <a target="_blank" href="wd.24st.com.cn">wd.24st.com.cn</a> ' +
+  ' | ' +
+  '<a target="_blank" href="https://github.com/accums">Made with❤by Accums</a>'
---
+++ docs/begin/begin.md
@@ -0,0 +1,10 @@
+---
+order: 1
+title: MySQL
+group:
+  title: 环境准备
+nav:
+  title: 开始使用
+  path: /1
+  order: 1
+---
